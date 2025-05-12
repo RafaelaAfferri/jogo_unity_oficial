acd6cfe8fe40d9a1c42635a25653975451dd6213
Ana Helena <anahc3@al.insper.edu.br>
2025-05-12 18:33:25 -0300

    cenas para fazer - gubs

--- jogo_2d_oficial/Assets/Scripts/SceneController.cs
+++ jogo_2d_oficial/Assets/Scripts/SceneController.cs
@@ -3,10 +3,22 @@ using UnityEngine.SceneManagement;
 
 public class SceneController : MonoBehaviour
 {
-    public string cena1 = "Esther";  // coloque aqui o nome exato da sua cena
+    public string sala1 = "Esther";  
+    public string config = "Config - gubs";  
+    public string controls = "Controles - gubs";  
 
     public void Jogar()
     {
-        SceneManager.LoadScene(cena1);
+        SceneManager.LoadScene(sala1);
+    }
+
+    public void Config()
+    {
+        SceneManager.LoadScene(config);
+    }
+
+    public void Controls()
+    {
+        SceneManager.LoadScene(controls);
     }
 }
